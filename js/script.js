1479a194dbb78be20210502c8e2e932e01daeb5e
LinaSeverus <severus@ukr.net>
2021-10-04 19:40:12 +0300

    init

---
+++ js/script.js
@@ -0,0 +1,39 @@
+'use strict'
+
+// Task 1
+
+let user = {};
+
+user.name = 'John';
+user.surName = 'Smith';
+// console.log(user);
+
+user.name = 'Pete';
+// console.log(user);
+
+delete user.name;
+// console.log(user);
+
+
+// Task 2   В данном случае нельзя менять константу user, но можно менять содержимое объекта. Поэтому код ниже работать будет)
+
+// const user = {
+//   name: 'John'
+// };
+
+// user.name = 'Pete';   
+
+
+// Task 3
+
+let salaries = {
+
+    John: 100,
+
+    Ann: 160,
+
+    Pete: 130
+
+}
+let sum = salaries.John + salaries.Ann + salaries.Pete;
+console.log(sum);
